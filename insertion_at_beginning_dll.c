9a00a32e7926926a26e04b66b8c0d3ec5f0c25cd
Vaishnavi Chirmade <vaishnavi.chirmade@gmail.com>
2025-05-08 13:03:21 +0530

    DSA_Programs

---
+++ insertion_at_beginning_dll.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node 
+{
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+//Function to create a new node
+struct Node * CreateNode(int new_data) 
+{
+    struct Node * new_node = (struct Node*)malloc(sizeof(struct Node));
+
+    new_node -> data = new_data;
+    new_node -> next = NULL;
+    new_node -> prev = NULL;
+
+    return new_node;
+}
+
+//Function to insert a node at the beginning
+struct Node * insert(struct Node * head, int new_data) 
+{
+    struct Node * new_node = CreateNode(new_data);
+
+    new_node -> next = head;
+
+    if(head != NULL) 
+    {
+        head -> prev = new_node;
+    }
+
+    return new_node;
+}
+
+// Function to print the list forward
+void print_list(struct Node * head) 
+{
+    struct Node * current = head;
+
+    printf("After Inserting Nodes : ");
+
+    while(current != NULL) 
+    {
+        printf("%d ", current -> data);
+
+        current = current -> next;
+    }
+
+    printf("\n");
+}
+
+int main() 
+{
+    struct Node * head = NULL;
+
+    head = insert(head, 30);
+    head = insert(head, 20);
+    head = insert(head, 10);
+
+    print_list(head);
+
+    return 0;
+}
