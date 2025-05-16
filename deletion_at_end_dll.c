95f7ebe442d803da01401457436ab850bdd96659
Vaishnavi Chirmade <vaishnavi.chirmade@gmail.com>
2025-05-16 14:06:13 +0530

    DSA_Programs

---
+++ deletion_at_end_dll.c
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node* prev;
+    struct Node* next;
+};
+
+//Function to create a new node
+struct Node * CreateNode(int new_data)
+{
+    struct Node * new_node = (struct Node*)malloc(sizeof(struct Node));
+
+    new_node -> data = new_data;
+    new_node -> prev = NULL;
+    new_node -> next = NULL;
+
+    return new_node;
+}
+
+//Function to insert a node at the beginning
+struct Node * insert(struct Node * head, int new_data)
+{
+    struct Node * new_node = CreateNode(new_data);
+
+    new_node -> next = head;
+
+    if(head != NULL)
+    {
+        head -> prev = new_node;
+    }
+
+    return new_node;
+}
+
+//Function to delete the node at the end
+struct Node * delete(struct Node * head)
+{
+    if(head == NULL)
+    {
+        printf("List is Empty!\n");
+
+        return NULL;
+    }
+
+    struct Node * temp = head;
+
+    while(temp -> next != NULL)
+    {
+        temp = temp -> next;
+    }
+
+    if(temp -> prev != NULL)
+    {
+        temp -> prev -> next = NULL;
+    }
+
+    else
+    {
+        head = NULL;
+    }
+
+    printf("Deleted Node : %d\n", temp -> data);
+
+    free(temp);
+
+    return head;
+}
+
+//Function to print the linked list
+void print_list(struct Node * head)
+{
+    struct Node * current = head;
+
+    while(current != NULL)
+    {
+        printf("%d <-> ", current -> data);
+
+        current = current -> next;
+    }
+
+    printf("NULL\n");
+}
+
+int main()
+{
+    struct Node * head = NULL;
+
+    head = insert(head, 3);
+    head = insert(head, 2);
+    head = insert(head, 1);
+
+    printf("Original List : ");
+
+    print_list(head);
+
+    head = delete(head);
+
+    printf("After Deleting Node : ");
+
+    print_list(head);
+
+    return 0;
+}
